5d5cf167df18a02ba231a1b337f521a6d0125f04
AZhan <xigchn@163.com>
2026-02-27 18:58:13 +0800

    不再更新golang

--- scripts/diy-2-packages.sh
+++ scripts/diy-2-packages.sh
@@ -16,46 +16,18 @@ function git_sparse_clone() {
 
 # ---- 移除 feeds 中的旧版包（防止与 package/ 下的新版冲突）----
 rm -rf feeds/packages/net/sing-box
-rm -rf feeds/packages/net/open-app-filter
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/net/frp
-rm -rf feeds/packages/lang/golang
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-aurora
 rm -rf feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/luci/applications/luci-app-aurora-config
-rm -rf feeds/luci/applications/luci-app-wechatpush
-rm -rf feeds/luci/applications/luci-app-appfilter
-rm -rf feeds/luci/applications/luci-app-frpc
-rm -rf feeds/luci/applications/luci-app-frps
-
-# ---- golang 工具链（sing-box/frp/ariang 等 Go 包的编译依赖）----
-git_sparse_clone master https://github.com/laipeng668/packages lang/golang
 
 # ---- sing-box 最新版 ----
 git_sparse_clone main https://github.com/sbwml/openwrt_helloworld net/sing-box
 
-# ---- ariang & frp ----
-git_sparse_clone ariang https://github.com/laipeng668/packages net/ariang
-git_sparse_clone frp https://github.com/laipeng668/packages net/frp
-git_sparse_clone frp https://github.com/laipeng668/luci applications/luci-app-frpc applications/luci-app-frps
-
 # ---- 主题 ----
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
 
 # ---- 额外插件 ----
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
-git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 package/openlist2
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
-git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
-git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
-git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led \
-         package/luci-app-athena-led/root/usr/sbin/athena-led
 
 # ---- 科学上网插件 ----
 # git_sparse_clone ...
@@ -65,12 +37,11 @@ chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led \
 # git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 
 # ---- 修复 package/ 下各包的 Makefile 路径引用 ----
-# golang-package.mk 路径从相对路径改为绝对路径，解决 yq 等 Go 包找不到工具链的问题
-find package/*/ -maxdepth 2 -path "*/Makefile" | \
-  xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
+# find package/*/ -maxdepth 2 -path "*/Makefile" | \
+#   xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
 # luci.mk 路径修正
-find package/*/ -maxdepth 2 -path "*/Makefile" | \
-  xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
+# find package/*/ -maxdepth 2 -path "*/Makefile" | \
+#   xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
 
 # ---- 统一更新并安装所有 feeds（包括 package/ 下新增的包）----
 ./scripts/feeds update -a
