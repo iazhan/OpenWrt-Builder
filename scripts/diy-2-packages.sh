438001129e82a90896f11dbf8d265e102fd61477
AZhan <xigchn@163.com>
2026-02-27 13:17:25 +0800

    不再替换golang

--- scripts/diy-2-packages.sh
+++ scripts/diy-2-packages.sh
@@ -17,29 +17,16 @@ function git_sparse_clone() {
 # ---- 移除 feeds 中的旧版包，替换为最新版 ----
 rm -rf feeds/packages/net/sing-box
 rm -rf feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/luci/applications/luci-app-wechatpush
-rm -rf feeds/luci/applications/luci-app-appfilter
-rm -rf feeds/luci/applications/luci-app-frpc
-rm -rf feeds/luci/applications/luci-app-frps
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/packages/net/open-app-filter
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/net/frp
-rm -rf feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
 
 # ---- sing-box 最新版（放回 feeds 目录）----
 git_sparse_clone main https://github.com/sbwml/openwrt_helloworld net/sing-box
 mv -f package/sing-box feeds/packages/net/sing-box
 
 # ---- ariang & golang & frp（放回 feeds 目录）----
-git_sparse_clone ariang https://github.com/laipeng668/packages net/ariang
-git_sparse_clone master https://github.com/laipeng668/packages lang/golang
-mv -f package/golang feeds/packages/lang/golang
-git_sparse_clone frp https://github.com/laipeng668/packages net/frp
-mv -f package/frp feeds/packages/net/frp
-git_sparse_clone frp https://github.com/laipeng668/luci applications/luci-app-frpc applications/luci-app-frps
-mv -f package/luci-app-frpc feeds/luci/applications/luci-app-frpc
-mv -f package/luci-app-frps feeds/luci/applications/luci-app-frps
+# git_sparse_clone master https://github.com/laipeng668/packages lang/golang
+# mv -f package/golang feeds/packages/lang/golang
 
 # ---- 主题 ----
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
